fda308c5bbe1f70772bbc1c02afbfed0e971458d
RichaLimbachiya <richalimbachiya2004@gmail.com>
2025-05-13 13:13:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+    // Trim leading spaces first
+    str = str.trimStart();
+
+    // Find the index of the first space
+    const spaceIndex = str.indexOf(' ');
+
+    // If there's no space, return the entire string
+    if (spaceIndex === -1) {
+        return str;
+    }
+
+    // Otherwise, return the substring from start to the first space
+    return str.substring(0, spaceIndex);
 }
-
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
